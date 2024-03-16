5106183b670aab8992dfff2cd376389464cd49fb
BISMAGHANI <159704908+BISMAGHANI@users.noreply.github.com>
2024-03-17 00:39:18 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer"
+
+// Asking Question from Users through Inquier
+
+let answers = await inquirer.prompt([
+    { message :   "enter first number", type : "number", name : "firstNumber"},
+    { message :   "enter second number", type : "number", name : "secondNumber"},
+
+    { message: "select one operator to perform operatons",
+      type: "list",
+      name: "operator",
+      choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+}
+]);
+
+console.log(answers);
+
+// conditional statement If-Else
+
+if(answers.operator ===  "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if(answers.operator ===  "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if(answers.operator ===  "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+if(answers.operator ===  "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("invalid input")
+}
+
+
